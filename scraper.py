1c4b000e54e793119aad80aae0fb077dcfe1bf03
Adrianov <adrianovriadi@gmail.com>
2024-07-17 15:14:24 +0700

    Added Blockcypher to API list

--- scraper.py
+++ scraper.py
@@ -29,7 +29,7 @@ def preprocess_csv():
     dataset.to_csv(file_path, index=False)
     return dataset
 
-def make_request(tx_id, tx_hash, proxies):
+def make_request_randomized(tx_id, tx_hash, proxies):
     source = os.getenv('SOURCE')
     tries = 0
     while True:
@@ -46,6 +46,8 @@ def make_request(tx_id, tx_hash, proxies):
                 res_json = scraper_electrumrpc.fetchTx(tx_hash, os.getenv('RPC_URL'), os.getenv('RPC_USER'), os.getenv('RPC_PASSWORD'))
             elif source == 'BLOCKSTREAM':
                 res_json = scraper_blockstreaminfo.fetchTx(tx_hash, proxy_dict)
+            elif source == 'BLOCKCYPHER':
+                res_json = scraper_blockcypher.fetchTx(tx_hash, proxy_dict)
             if res_json:
                 print(f"{tx_id} - Request success using proxy: {proxy}")
                 return {'tx_id': tx_id, 'tx_hash': tx_hash,'scraped': True, 'proxyAddr': proxy, 'res': res_json, 'source': source}
@@ -62,23 +64,26 @@ if __name__ == "__main__":
     df = preprocess_csv()
     workers = int(os.getenv('NUM_WORKER'))
     proxies = fetch_proxies.load_proxies(os.path.join(os.getcwd(), 'proxies_list.txt'))
-    for i in range(int(os.getenv('MAX_EPOCH'))):
-        results = []
-        with concurrent.futures.ThreadPoolExecutor(max_workers=workers) as executor:
-            df_chunks = df[df['tried'] == False].head(workers)
-            futures = [executor.submit(make_request, int(df_chunks.iloc[i]['txId']), df_chunks.iloc[i]['transaction'], proxies) for i in range(workers)]
-            for future in concurrent.futures.as_completed(futures):
-                filename = f"{future.result()['tx_hash']}_{future.result()['source']}.json"
-                filepath = os.path.join(os.getenv('OUTPUT_DIR'), filename)
-                with open(filepath, 'w') as json_file:
-                    json.dump(future.result(), json_file)
-                results.append(future.result())
-                
-        csv_file = os.path.join(os.getenv('OUTPUT_DIR'), os.getenv('TXS_LIST'))
-        for res in results:
-            df.loc[df['txId'] == res['tx_id'], 'scraped'] = res['scraped']
-            df.loc[df['txId'] == res['tx_id'], 'proxyAddr'] = res['proxyAddr']
-            df.loc[df['txId'] == res['tx_id'], 'tried'] = True
-            df.loc[df['txId'] == res['tx_id'], 'source'] = res['source']
-            df.loc[df['txId'] == res['tx_id'], 'last_updated'] = time.time()
-        df.to_csv(csv_file, index=False)
+    scraping_scheme = os.gentenv('SCRAPING_SCHEME').lower()
+    if scraping_scheme == 'randomized':
+        for i in range(int(os.getenv('MAX_EPOCH'))):
+            results = []
+            with concurrent.futures.ThreadPoolExecutor(max_workers=workers) as executor:
+                df_chunks = df[df['tried'] == False].head(workers)
+                futures = [executor.submit(make_request_randomized, int(df_chunks.iloc[i]['txId']), df_chunks.iloc[i]['transaction'], proxies) for i in range(workers)]
+                for future in concurrent.futures.as_completed(futures):
+                    filename = f"{future.result()['tx_hash']}_{future.result()['source']}.json"
+                    filepath = os.path.join(os.getenv('OUTPUT_DIR'), filename)
+                    with open(filepath, 'w') as json_file:
+                        json.dump(future.result(), json_file)
+                    results.append(future.result())
+                    
+            csv_file = os.path.join(os.getenv('OUTPUT_DIR'), os.getenv('TXS_LIST'))
+            for res in results:
+                df.loc[df['txId'] == res['tx_id'], 'scraped'] = res['scraped']
+                df.loc[df['txId'] == res['tx_id'], 'proxyAddr'] = res['proxyAddr']
+                df.loc[df['txId'] == res['tx_id'], 'tried'] = True
+                df.loc[df['txId'] == res['tx_id'], 'source'] = res['source']
+                df.loc[df['txId'] == res['tx_id'], 'last_updated'] = time.time()
+            df.to_csv(csv_file, index=False)
+    elif scraping_scheme == 'max_rate':
---
+++ scraper_blockcypher.py
@@ -0,0 +1,69 @@
+import requests
+import json
+import time
+
+def fetchTx(tx_id, proxy=None):
+    # Base URL for the API
+    base_url = 'https://api.blockcypher.com/v1/btc/main/txs/'
+    
+    # Complete URL with the transaction ID
+    url = f'{base_url}{tx_id}'
+    try:
+        # Send the GET request
+        response = requests.get(url, proxies=proxy)
+        
+        # Check if the request was successful
+        if response.status_code == 200:
+            # Parse the response content as JSON
+            json_data = response.json()
+            return json_data
+        else:
+            print(f"Request failed with status code: {response.status_code}")
+            return None
+    except requests.exceptions.RequestException as e:
+        print(f"An error occurred: {e}")
+        return None
+
+
+def fetchAddrHist(addr, proxy=None):
+    # Base URL for the API
+    base_url = 'https://api.blockcypher.com/v1/btc/main/addrs/'
+    
+    # Complete URL with the Bitcoin address
+    url = f'{base_url}{addr}'
+    
+    try:
+        # Send the GET request
+        response = requests.get(url)
+        
+        # Check if the request was successful
+        if response.status_code == 200:
+            # Parse the response content as JSON
+            json_data = response.json()
+            return json_data
+        else:
+            print(f"Request failed with status code: {response.status_code}")
+            return None
+    except requests.exceptions.RequestException as e:
+        print(f"An error occurred: {e}")
+        return None
+
+if __name__ == "__main__":
+    tx_id = '5658a925ca3a5528353ae9324263d688c8b5932eac4261dae035bf948deb0f8d'
+    start_time = time.time()
+    transaction_details = fetchTx(tx_id)
+    end_time = time.time()
+    duration = end_time - start_time
+    if transaction_details:
+        print(transaction_details)
+        print(f'Results fetched in {duration} s')
+        print("-----------------------")
+    bitcoin_address = '3AujBhj7bAFgGVb8gYccUDpWcAYYUJ37WY'
+    start_time = time.time()
+    address_details = fetchAddrHist(bitcoin_address)
+    end_time = time.time()
+    duration = end_time - start_time
+    if address_details:
+        print(address_details)
+        print(f'Results fetched in {duration} s')
+        print("-----------------------")
